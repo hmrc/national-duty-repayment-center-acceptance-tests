ae75cce7d657ef7521da5bf5588746b743fb1662
Lorenzo Mugnai <11977337+lmugnai@users.noreply.github.com>
2021-04-21 22:07:49 +0100

    update app dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val dependencies = Seq(
-    "uk.gov.hmrc"                %% "webdriver-factory"       % "0.+" % Test,
+    "uk.gov.hmrc"                %% "webdriver-factory"       % "0.18.0" % Test,
     "org.scalatest"              %% "scalatest"               % "3.0.8"  % Test,
     "org.scalactic"              %%  "scalactic"              % "3.0.8",
     "org.pegdown"                %   "pegdown"                % "1.6.0",
@@ -12,7 +12,7 @@ object Dependencies {
     "io.cucumber"                %   "cucumber-picocontainer" % "2.0.1",
     "junit"                      %  "junit"                   % "4.12"  % Test,
     "com.novocode"               %  "junit-interface"         % "0.11"  % Test,
-    "uk.gov.hmrc"                %% "zap-automation"          % "2.7.0" % Test,
+    "uk.gov.hmrc"                %% "zap-automation"          % "2.9.0" % Test,
     "org.apache.commons"         %   "commons-lang3"          % "3.11",
     "com.typesafe"               %  "config"                  % "1.3.2" % Test
   )
